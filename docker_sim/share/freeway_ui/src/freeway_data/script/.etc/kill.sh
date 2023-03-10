9277d764b2068c7cc6fba273228f1fcd723eda8c
jay <jhjo1997@naver.com>
2023-03-10 16:54:56 +0900

    fc

---
+++ docker_sim/share/freeway_ui/src/freeway_data/script/.etc/kill.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+source /home/fw05/.zshrc
+
+echo "Launching Application, please wait!"
+#--tab --title="ros core kill" -e "zsh -c \"killall -9 roscore;exec zsh\"" \
+#--tab --title="ros master kill" -e "zsh -c \"killall -9 rosmaster;exec zsh\"" \
+#--tab --title="kill terminal" -e "zsh -c \"killall -3 gnome-shell;exec zsh\""
+#gnome-terminal --geometry=40x40  \
+#--tab --title="ros node kill" -e "zsh -c \"rosnode kill -a;exec zsh\"" \
+#--tab --title="ros node kill" -e "zsh -c \"reboot;exec zsh\""
+
+
+gnome-terminal --geometry=40x40  \
+--tab --title="ros node kill" -e "zsh -c \"rosnode kill -a;sleep 2s;rosnode kill -a;exec zsh\"" \
+#--tab --title="ros master kill" -e "zsh -c \"sleep 5s;killall -9 rosmaster;exec zsh\"" \
+#--tab --title="ros core kill" -e "zsh -c \"sleep 5s;killall -9 roscore;exec zsh\""
